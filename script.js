bf05599d4f6b9f1289f83f0f3d4400c9e441441d
HostingbyVinlandDelsgade <keydnavi@gmail.com>
2024-06-10 15:42:50 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
 $(document).ready(function() {
-    $(window).scroll(function() {
-        $(".slideanim").each(function() {
-            var pos = $(this).offset().top;
-            var winTop = $(window).scrollTop();
-            if (pos < winTop + $(window).height() - 50) {
-                $(this).addClass("slide");
-            }
-        });
-    });
-});
+  // Scroll to the top of the page when the document is ready
+  $(window).scrollTop(0);
 
-
-$(document).ready(function () {
-    $(".navbar a, footer a[href='#myPage']").on("click", function (event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $("html, body").animate(
-          {
-            scrollTop: $(hash).offset().top,
-          },
-          900,
-          function () {
-            window.location.hash = hash;
+  $(window).scroll(function() {
+      $(".slideanim").each(function() {
+          var pos = $(this).offset().top;
+          var winTop = $(window).scrollTop();
+          if (pos < winTop + $(window).height() - 50) {
+              $(this).addClass("slide");
           }
-        );
-      } 
-    });
+      });
+  });
+
+  $(".navbar a, footer a[href='#myPage']").on("click", function(event) {
+      if (this.hash !== "") {
+          event.preventDefault();
+          var hash = this.hash;
+          $("html, body").animate({
+              scrollTop: $(hash).offset().top
+          }, 900, function() {
+              window.location.hash = hash;
+          });
+      }
+  });
 });
